6f90a7a259d33dfaa65f538c95877574320ca6b1
Imhwan <ynw2341@naver.com>
2025-06-08 14:29:39 +0900

    SCG, configserver, eurekaserver 추가

---
+++ infra/gateway/src/main/java/com.msa.gatewayServer/ScgApplication.java
@@ -0,0 +1,15 @@
+package com.msa.gatewayServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ScgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScgApplication.class, args);
+    }
+
+}
--- microservices/order-service/src/main/java/com/msa/delivery/OrderServiceApplication.java
+++ microservices/order-service/src/main/java/com/msa/delivery/OrderServiceApplication.java
@@ -2,10 +2,12 @@ package com.msa.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.msa")
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
